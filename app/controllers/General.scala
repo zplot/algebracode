feafb050b5b0269e7fcba6bc6146fa469d1f507c
Luis Fontes <luis2@zplot.com>
2016-03-12 09:15:19 +0100

    save

--- app/controllers/General.scala
+++ app/controllers/General.scala
@@ -9,7 +9,7 @@ import models.algebra.FiniteGroupExamples._
 
 import play.api._
 import play.api.mvc._
-import play.api.data.{ Form }
+import play.api.data.Form
 import play.api.data.Forms._
 
 
@@ -17,35 +17,71 @@ import play.api.data.Forms._
 
 object General extends Controller {
 
-  val toPresentErrors = {
+  def inCaseOfErrors(key: String): GeneralEntryForm.Form1 = {
 
-    val key: String = "generic"
-    val title: String = "Test"
-    val title5fields: String = "Test"
-    val label1: String = "Please review"
-    val subject: String = "Test"
-    val project: String = "Test"
-    val references: String = "Test"
-    val text1: String = "Test"
-    val text2: String = "Test"
+    val salida = key match {
 
-    GeneralEntryForm.Form1(
-      key,
-      title,
-      title5fields,
-      label1,
-      subject,
-      project,
-      references,
-      text1,
-      text2
-    )
+      case "generic" => {
 
 
+        val title: String = "Test"
+        val title5fields: String = "Test"
+        val label1: String = "Please review"
+        val subject: String = "Test"
+        val project: String = "Test"
+        val references: String = "Test"
+        val text1: String = "Test"
+        val text2: String = "Test"
+
+        GeneralEntryForm.Form1(
+          key,
+          title,
+          title5fields,
+          label1,
+          subject,
+          project,
+          references,
+          text1,
+          text2
+        )
+
+      }
+
+      case "cayley" => {
+
+        val title: String = "Caley Table of a Permutation Group"
+        val title5fields: String = "Caley Table of a Permutation Group"
+        val label1: String = "Enter the name of a valid finite group"
+        val subject: String = "Permutation Groups"
+        val project: String = "Cayley Table"
+        val references: String = "These are the references on this subject"
+        val text1: String = "What a permutation group is"
+        val text2: String = "What a Cayley Table is"
+
+        GeneralEntryForm.Form1(
+
+          key,
+          title,
+          title5fields,
+          label1,
+          subject,
+          project,
+          references,
+          text1,
+          text2
+        )
+      }
+
+    }
+
+    salida
   }
 
 
-  // Crea el form para introducir datos
+
+
+
+  // Crea el form para introducir datos para cada aplicación identificada por key
   def createForm(key: String) = Action {
 
     val salida = key match {
@@ -74,30 +110,30 @@ object General extends Controller {
         par
       }
 
-        case "cayley" => {
+      case "cayley" => {
 
-          val title: String = "Caley Table of a Permutation Group"
-          val title5fields: String = "Caley Table of a Permutation Group"
-          val label1: String = "Enter the name of a valid finite group"
-          val subject: String = "Permutation Groups"
-          val project: String = "Cayley Table"
-          val references: String = "These are the references on this subject"
-          val text1: String = "What a permutation group is"
-          val text2: String = "What a Cayley Table is"
+        val title: String = "Caley Table of a Permutation Group"
+        val title5fields: String = "Caley Table of a Permutation Group"
+        val label1: String = "Enter the name of a valid finite group"
+        val subject: String = "Permutation Groups"
+        val project: String = "Cayley Table"
+        val references: String = "These are the references on this subject"
+        val text1: String = "What a permutation group is"
+        val text2: String = "What a Cayley Table is"
 
-          val par = GeneralEntryForm.Form1(
-            key,
-            title,
-            title5fields,
-            label1,
-            subject,
-            project,
-            references,
-            text1,
-            text2
-          )
-          par
-        }
+        val par = GeneralEntryForm.Form1(
+          key,
+          title,
+          title5fields,
+          label1,
+          subject,
+          project,
+          references,
+          text1,
+          text2
+        )
+        par
+      }
     }
 
     Ok(views.html.generalinput(salida))
@@ -113,140 +149,143 @@ object General extends Controller {
     input1fields.bindFromRequest.fold(
 
       // Hay errores
-      formWithErrors => Ok(views.html.errorsview(toPresentErrors)),
+      formWithErrors => Ok(views.html.errorsview(inCaseOfErrors(key))),
 
       // No hay errores
+      // A la view de presentación de resultados se le pasan los valores que debe presentar: process(key, value)
       value => Ok(views.html.generalblackboard(process(key, value))))
+
   }
 
 
 
 
-  // En Left tendremos el error caso de que exista
-  // En Right tendremos el completo de lo que se va a presentar
-  def process(key: String, value: GeneralEntryForm.Form2): GeneralOutput.PageContent = {
+
+    // Se le pasa un key que identifica la aplicación: key
+    // Se le pasa los dotos introducidos por el usuario: value
+    // Devuelve los datos que hay que mostrar en la página de resultados. Se guardan en: salida
+    def process(key: String, value: GeneralEntryForm.Form2): GeneralOutput.PageContent = {
+
+      val salida = key match {
+
+        case "generic" => {
+
+          val titular = "Este es el título de los resultados a mostrar"
+          val numLineas = 5
+          val contenido = List(List("1", "2", "3"), List("4", "5", "6"), List("7", "8", "9"))
+
+          val key = "cayley"
+          val title = "titulo"
+          val notebook = "notebook"
+          val page = "page"
+          val next = "next"
+          val previous ="previous"
+          val subject = "subject"
+          val project = "project"
+          val references = "http://www.hp.com"
+          val text1 = "text1"
+          val text2 = "text2"
+
+          val camposExtra = GeneralOutput.ExtraFields(
+            key,
+            title,
+            notebook,
+            page,
+            next,
+            previous,
+            subject,
+            project,
+            references,
+            text1,
+            text2
+          )
+
+          GeneralOutput.PageContent(
+            camposExtra,
+            titular,
+            numLineas,
+            contenido
+          )
+        }
+
+        case "cayley" => {
+
+          def fromGroupStringToCayleyTableOK(s: String): Either[String, List[List[String]]]  = {
+            val t = fromStringToGroup(s)
+            t match {
+              case Right(x)  => Right(x.cayleyTableOK)
+              case Left(x) => Left(x)
+            }
+
+          }
+
+          val titular = {
+            val resultado: Either[String, List[List[String]]] = fromGroupStringToCayleyTableOK(value.input1)
+            val output = resultado match {
+              case Left(x) => x
+              case Right(x) => "The Cayley table of selected group is:"
+            }
+            output
+          }
+          val numLineas = {
+            val resultado: Either[String, List[List[String]]] = fromGroupStringToCayleyTableOK(value.input1)
+            val output = resultado match {
+              case Left(x) => 0
+              case Right(x) => x.length
+            }
+            output
+          }
+          val contenido = {
+            val resultado: Either[String, List[List[String]]] = fromGroupStringToCayleyTableOK(value.input1)
+            val output = resultado match {
+              case Left(x) => List(List())
+              case Right(x) => x
+            }
+            output
+          }
+
+          val key = "cayley"
+          val title = "Caley Table of a Permutation Group"
+          val notebook = "Finite Groups"
+          val page = "Cayley Table computation"
+          val next = "next"
+          val previous ="previous"
+          val subject = "Permutation Groups"
+          val project = "Cayley Table"
+          val references = "http://www.hp.com"
+          val text1: String = "A permutation group is a group G whose elements are permutations of a given set M and whose group operation is the composition of permutations in G (which are thought of as bijective functions from the set M to itself)."
+          val text2: String = "If M = {1,2,...,n} then, S(M), the symmetric group on n letters is usually denoted by Sn. We write it in this site as S(n)"
 
 
+          val camposExtra = GeneralOutput.ExtraFields(
+            key,
+            title,
+            notebook,
+            page,
+            next,
+            previous,
+            subject,
+            project,
+            references,
+            text1,
+            text2
+          )
 
-    val salida = key match {
-
-      case "generic" => {
-
-        val titular = "Este es el título de los resultados a mostrar"
-        val numLineas = 5
-        val contenido = List(List("1", "2", "3"), List("4", "5", "6"), List("7", "8", "9"))
-
-        val key = "cayley"
-        val title = "titulo"
-        val notebook = "notebook"
-        val page = "page"
-        val next = "next"
-        val previous ="previous"
-        val subject = "subject"
-        val project = "project"
-        val references = "http://www.hp.com"
-        val text1 = "text1"
-        val text2 = "text2"
-
-        val camposExtra = GeneralOutput.ExtraFields(
-                                                      key,
-                                                      title,
-                                                      notebook,
-                                                      page,
-                                                      next,
-                                                      previous,
-                                                      subject,
-                                                      project,
-                                                      references,
-                                                      text1,
-                                                      text2
-        )
-
-        GeneralOutput.PageContent(
-                                    camposExtra,
-                                    titular,
-                                    numLineas,
-                                    contenido
-                                  )
+          GeneralOutput.PageContent(
+            camposExtra,
+            titular,
+            numLineas,
+            contenido
+          )
+        }
       }
 
-      case "cayley" => {
+      salida
 
-        def fromGroupStringToCayleyTableOK(s: String): Either[String, List[List[String]]]  = {
-          val t = fromStringToGroup(s)
-          t match {
-            case Right(x)  => Right(x.cayleyTableOK)
-            case Left(x) => Left(x)
-          }
-
-        }
-
-        val titular = {
-          val resultado: Either[String, List[List[String]]] = fromGroupStringToCayleyTableOK(value.input1)
-          val output = resultado match {
-            case Left(x) => x
-            case Right(x) => "The Cayley table of selected group is:"
-          }
-          output
-        }
-        val numLineas = {
-          val resultado: Either[String, List[List[String]]] = fromGroupStringToCayleyTableOK(value.input1)
-          val output = resultado match {
-            case Left(x) => 0
-            case Right(x) => x.length
-          }
-          output
-        }
-        val contenido = {
-          val resultado: Either[String, List[List[String]]] = fromGroupStringToCayleyTableOK(value.input1)
-          val output = resultado match {
-            case Left(x) => List(List())
-            case Right(x) => x
-          }
-          output
-        }
-
-        val key = "cayley"
-        val title = "Caley Table of a Permutation Group"
-        val notebook = "Finite Groups"
-        val page = "Cayley Table computation"
-        val next = "next"
-        val previous ="previous"
-        val subject = "Permutation Groups"
-        val project = "Cayley Table"
-        val references = "http://www.hp.com"
-        val text1: String = "A permutation group is a group G whose elements are permutations of a given set M and whose group operation is the composition of permutations in G (which are thought of as bijective functions from the set M to itself)."
-        val text2: String = "If M = {1,2,...,n} then, S(M), the symmetric group on n letters is usually denoted by Sn. We write it in this site as S(n)"
-
-
-        val camposExtra = GeneralOutput.ExtraFields(
-          key,
-          title,
-          notebook,
-          page,
-          next,
-          previous,
-          subject,
-          project,
-          references,
-          text1,
-          text2
-        )
-
-        GeneralOutput.PageContent(
-          camposExtra,
-          titular,
-          numLineas,
-          contenido
-        )
-      }
     }
 
-  salida
 
   }
-
-}
 
 
 
